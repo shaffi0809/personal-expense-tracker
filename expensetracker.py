1206b8281177ddd73816901c87176a4e813065ed
sk shaffi <shaffi0809@gmail.com>
2025-12-10 07:49:45 +0000

    expense tracker is ready

--- expensetracker.py
+++ expensetracker.py
@@ -1,50 +1,49 @@
 expenses=[]
 def add_expense():
     amount=float(input("enter amount :"))
-    category=input("enter category(food,travel,shopping): ")
+    category=input("enter category  (food,travel,shopping) :")
     note=input("enter note :")
-    expense={
+    expense = {
         'amount':amount,
         'category':category,
         'note':note
     }
     expenses.append(expense)
-    print("expense added succesfully!\n")
-def view_expenses():
+    print("expense is added sucesully :")
+def view_expense():
     if not expenses:
-        print("no expenses are found !\n")
+        print("invalid expenses:")
         return
-    print("\n.......all expense..........")
+    print("\n...............all expenses...............")
     for i,exp in enumerate(expenses,1):
-        print(f"{i}.${exp['amount']} | {exp['category']} | {exp['note']}")
-        print()
+        print(f"{i}.${exp['amount']} | {exp['category']} | {exp["note"]}")
 def total_expense():
-    total=sum(e['amount'] for e in expenses)
-    print(f"total spent :{total}")
-def expense_by_category():
+    total=sum(s['amount'] for s in expenses)
+    print(f"taotal spent :{total}")
+def seperate_category():
     category=input("enter category :")
-    total=sum(e['amount']  for e in expenses if e['category'].lower()==category.lower())
-    print(f"expense by category {category}:{total}")
+    total=sum(s['amount'] for s in expenses if s['category'].lower()==category.lower())
+    print(f" category is {category},{total}")
+
 def main():
     while True:
-        print("........expense tracker..........")
-        print("1.Add expence")
-        print("2. View All expence")
-        print("3. total expence")
-        print("4. expence by category")
-        print("5. exit")
-        choice=input("enter ur choice :")
-        if choice=="1":
+        print("..........All expenses..............\n")
+        print("1.add expense")
+        print("2.view expense")
+        print("3.total expense")
+        print("4.seperate category")
+        print("5.exit")
+        choice=input("enter ur option :")
+        if choice == "1":
             add_expense()
-        elif choice=="2":
-            view_expenses()
-        elif choice=="3":
+        elif choice == "2":
+            view_expense()
+        elif choice == "3":
             total_expense()
-        elif choice=="4":
-            expense_by_category()
-        elif choice=="5":
-            print("thnak u for choosing this expense tracker")
-            break
+        elif choice == "4":
+            seperate_category()
+        elif choice == "5":
+            print("thank u for choose this tracker")
         else:
-            print("invalid")
+            print("invalid please try again :")
 main()
